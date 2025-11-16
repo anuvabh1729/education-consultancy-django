16c4331018386357daec738db856f3d723eca324
Anuvabh Sutradhar <anuvabhsutradhar@gmail.com>
2025-11-16 12:22:55 +0000

    updated admin.py

--- consultproject/consultants/admin.py
+++ consultproject/consultants/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ConsultantProfile, ConsultantSchedule
 
-# Register your models here.
+admin.site.register(ConsultantProfile)
+admin.site.register(ConsultantSchedule)

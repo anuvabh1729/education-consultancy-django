03c48523b133a357ac58b97e3489fc6cbd41c980
Anuvabh Sutradhar <anuvabhsutradhar@gmail.com>
2025-11-16 11:12:23 +0000

    updated admin.py

--- consultproject/students/admin.py
+++ consultproject/students/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (
+    StudentProfile,
+    StudentDocument,
+    UniversityApplication,
+    ConsultantBooking,
+)
 
-# Register your models here.
+admin.site.register(StudentProfile)
+admin.site.register(StudentDocument)
+admin.site.register(UniversityApplication)
+admin.site.register(ConsultantBooking)
